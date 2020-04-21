e0824a2bc424378a01ffda6c4f101c53c5cc0dcc
ruigang19289 <64006267+ruigang19289@users.noreply.github.com>
2020-04-21 17:24:29 +0800

    Add files via upload

---
+++ aliyun.sh
@@ -0,0 +1,4 @@
+#/bin/bash 
+curl -o  /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+curl -o  /etc/yum.repos.d/Centos-7.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+sed -i 's/gpgcheck=1/gpgcheck=0/' /etc/yum.repos.d/*.repo
